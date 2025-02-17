7d9f928db221aba1cc958cc0bb84bcd6adae688a
Noorhesham <noordragon2004@gmail.com>
2025-02-17 21:28:29 +0200

    setting things up

---
+++ app/components/app-sidebar.tsx
@@ -0,0 +1,78 @@
+"use client";
+import * as React from "react";
+
+import { SearchForm } from "./search-form";
+import { VersionSwitcher } from "./version-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+
+const data = {
+  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
+  navMain: [
+    {
+      title: "Building Your Application",
+      url: "/dashboard",
+      items: [
+        {
+          title: "Products",
+          url: "/dashboard/products",
+        },
+        {
+          title: "orders",
+          url: "/dashboard/orders",
+        },
+        {
+          title: "Create Product",
+          url: "/dashboard/createproduct",
+        },
+        {
+          title: "Global Variants",
+          url: "/dashboard/variants",
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const pathname = usePathname();
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <VersionSwitcher versions={data.versions} defaultVersion={data.versions[0]} />
+        <SearchForm />
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild isActive={pathname === item.url}>
+                      <a href={item.url}>{item.title}</a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
