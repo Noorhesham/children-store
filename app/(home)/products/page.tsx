2a5f60598a0e023f284baa61aecff4a18f2b4f85
Noorhesham <noordragon2004@gmail.com>
2025-02-21 15:21:18 +0200

    fix names

--- app/(home)/products/page.tsx
+++ app/(home)/products/page.tsx
@@ -1,8 +1,10 @@
 import ProductGrid from "@/app/components/ProductGrid";
 import ProductList from "@/app/components/ProductList";
+import connect from "@/app/utils/clientPromise";
 import React, { Suspense } from "react";
 
-const page = () => {
+const page = async () => {
+  await connect();
   return (
     <Suspense>
       <ProductList />
