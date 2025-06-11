3945f919eec0c51f57749ac70b642c7f87b00aa5
Bridge Add-in Developer <bridge-addin@example.com>
2025-06-11 12:24:35 +0300

    fix categories

--- app/dashboard/categories/page.tsx
+++ app/dashboard/categories/page.tsx
@@ -11,17 +11,32 @@ import CategoryForm from "@/app/components/forms/CategoryForm";
 
 export const dynamic = "force-dynamic";
 
-const Page = async () => {
+const Page = async ({ searchParams }: { searchParams: { page?: string } }) => {
   await connect();
 
-  const data = await Category.find({}).lean();
+  const currentPage = parseInt(searchParams.page || "1", 10);
+  const limit = 10;
+
+  const data = await Category.find({})
+    .limit(limit)
+    .skip((currentPage - 1) * limit)
+    .lean();
   const dataObj = JSON.parse(JSON.stringify(data));
+  const totalCount = (await Category.countDocuments({}).lean()) as number;
+  const totalPages = Math.ceil(totalCount / limit);
   return (
     <MaxWidthWrapper className="flex px-4 flex-col mt-5">
       <div className="flex items-center gap-2">
         <ModelCustom btn={<Button className="self-end">اضافة تصنيف</Button>} content={<CategoryForm />} />
       </div>
-      <DataTable handleDeleteAll={deleteEntity} columns={categoryColumns} data={dataObj} entity="Category" page={1} />
+      <DataTable
+        handleDeleteAll={deleteEntity}
+        columns={categoryColumns}
+        data={dataObj}
+        entity="Category"
+        page={currentPage}
+        totalPages={totalPages}
+      />
     </MaxWidthWrapper>
   );
 };
